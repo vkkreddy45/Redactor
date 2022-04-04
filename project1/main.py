2b05112db850705b8fdf4af28a1b5eeb17b08f3d
vudu0001 <vudu0001@ou.edu>
2022-04-04 05:44:48 +0000

    Added some Changes

--- project1/main.py
+++ project1/main.py
@@ -1,61 +1,59 @@
 # -*- coding: utf-8 -*-
 # Example main.py
 import argparse
-
 import project1
 
 def main(args):
-    # Getting the Input
+
+    # Getting the input data
     if args.input:
         print(args.input)
-        input_data = project1.Read_file(args.input)
+        input_data=project1.Read_file(args.input)
 
     if args.names:
     # Reading names
         input_data, count=project1.Redact_names(input_data)
     
     if args.dates:
-    # 
+    # Reading dates
         input_data, count=project1.Redact_dates(input_data)
     
     if args.phones:
-    #
+    # Reading phone numbers
         input_data, count=project1.Redact_phones(input_data)
     
     if args.genders:
-    # 
+    # Reading genders
         input_data,count=project1.Redact_gender(input_data)
 
     if args.address:
-    #
+    # Reading address
         input_data,count=project1.Redact_address(input_data)
 
     if args.concept:
-    #
+    # Reading concepts
         input_data,count=project1.Redact_concept(input_data, args.concept)
 
     if args.output:
-    #
+    # Writing data into a File
         project1.write_output(input_data)
 
     if args.stats:
-    # #
+    # Writing stats into a File
         project1.write_stats(input_data)
 
 
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input', required=True, help="Input Files", nargs='*', action='append')
-    parser.add_argument('--names', required=False, help='Redact_names', action='store_true') 
-    parser.add_argument('--dates', required=False, help='Redact_dates', action='store_true')
-    parser.add_argument('--phones', required=False, help='Redact_phones', action='store_true')
-    parser.add_argument('--genders', required=False, help='Redact_gender', action='store_true')
-    parser.add_argument('--address', required=False, help='Redact_address', action='store_true')
-    parser.add_argument('--concept', help='Redact_concept')
-    parser.add_argument('--output', help='write_output')
-    parser.add_argument('--stats', required=False, help='write_status')
-    args = parser.parse_args()
+if __name__=='__main__':
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--input', action='append', help="Input Files", nargs='*', required=True)
+    parser.add_argument('--names', action='store_true', help='Redact_names', required=False) 
+    parser.add_argument('--dates', action='store_true', help='Redact_dates', required=False)
+    parser.add_argument('--phones', action='store_true', help='Redact_phones', required=False )
+    parser.add_argument('--genders', action='store_true', help='Redact_gender', required=False)
+    parser.add_argument('--address', action='store_true', help='Redact_address', required=False)
+    parser.add_argument('--concept', help='Redact_concept', required=True)
+    parser.add_argument('--output', help='write_output', required=False)
+    parser.add_argument('--stats', help='write_status', required=False)
+    args=parser.parse_args()
 
     main(args)
--- project1/project1.py
+++ project1/project1.py
@@ -1,66 +1,66 @@
-import spacy 
+import glob 
+from itertools import chain
 import nltk
+from nltk.corpus import wordnet
 #nltk.download("wordnet")
 #nltk.download("omw-1.4")
-import glob
-from itertools import chain
-import regex as re
-from nltk.corpus import wordnet
 import os
+import regex as re
+import spacy
 
-nlp = spacy.load("en_core_web_sm")
+nlp=spacy.load("en_core_web_sm")
 input_files=[]
 
-# Function for Getting the input data from Glob
+# Function for getting data from glob
 
 def Read_file(path):
-    idata = []
-    files = list(chain.from_iterable(path))
+    idata=[]
+    files=list(chain.from_iterable(path))
 
     for i in range(len(files)):
-        finput = glob.glob(files[i])
+        finput=glob.glob(files[i])
         for j in range(len(finput)):
-            dat = open(finput[j]).read()
+            dat=open(finput[j]).read()
             idata.append(dat)
             #print(idata)
             input_files.append(finput[j])
     return idata
 
 def redact_names_with(token):
-    if token.ent_iob != 0 and token.ent_type_ == 'PERSON':
-        return '\u2588 '
+    if token.ent_iob!=0 and token.ent_type_=='PERSON':
+        return '\u2588'
     return str(token)
 
 def redact_names(nlp_doc):
-    # for ent in nlp_doc:
-    #     print(ent, ent.ent_type_)
-    tokens = map(redact_names_with, nlp_doc)
+    tokens=map(redact_names_with, nlp_doc)
     return ' '.join(tokens)
 
 # Function for Reading Names
+
 def Redact_names(idata):
     tdata=[]
     for text in idata:
-        doc = nlp(text)
+        doc=nlp(text)
         tdata.append(redact_names(doc))
     #print(tdata)
-    ncount = tdata[0].count('\u2588')
+    ncount=tdata[0].count('\u2588')
     #print(tdata,ncount)
     return tdata,ncount
 
-# Fuction for Genders
+# Fuction for Reading Genders
+
 def Redact_gender(idata):
     tdata=[]
     # Pronous for genders
-    gender=['he','she','him','her','his','hers','male','female','man','woman','men','women','He','She','Him','Her','His','Hers','Male','Female','Man','Woman','Men','Women','HE','SHE','HIM','HER','HIS','HERS','MALE','FEMALE','MAN','WOMAN','MEN','WOMEN']
+    gender=['female','man','woman','men','he','she','him','her','his','hers','male','women','He','She','Him','Her','His','Hers','Male','Female','Man','Woman','Men','Women','HE','SHE','HIM','HER','HIS','HERS','MALE','FEMALE','MAN','WOMAN','MEN','WOMEN']
     for i in idata:
-        doc = nlp(i)
-        temp = []
+        doc=nlp(i)
+        temp=[]
         for x in doc:
-            test = lambda x : '\u2589' if str(x) in gender else x
+            test=lambda x:'\u2589' if str(x) in gender else x
             temp.append(test(x))
         tdata.append(" ".join([str(x) for x in temp]))
-    gcount = tdata[0].count('\u2589')
+    gcount=tdata[0].count('\u2589')
     #print(tdata,gcount)
     return tdata,gcount
 
@@ -69,80 +69,77 @@ def Redact_gender(idata):
 def Redact_dates(idata):
     tdata=[]
     for i in idata:
-        doc = nlp(i)
-        dlist = []
+        doc=nlp(i)
+        dlist=[]
         for x in doc:
             if x.ent_type_=='DATE' or x.ent_type_=='CARDINAL':
                 dlist.append(x)
-        dlist = set(dlist)
-        temp = []
+        dlist=set(dlist)
+        temp=[]
         for t in doc:
-            test = lambda z : '\u2587' if z in dlist else z
+            test=lambda z:'\u2587' if z in dlist else z
             temp.append(test(t))
         tdata.append(" ".join([str(x) for x in temp]))
-    dcount = tdata[0].count('\u2587')
+    dcount=tdata[0].count('\u2587')
     #print(tdata,dcount)
     return tdata,dcount
-# Function for Reading Phones
+
+# Function for Reading Phones numbers
 
 def Redact_phones(idata):
-    #print(idata)
-    tdata = []
+    tdata=[]
     for i,text in enumerate(idata):
-        ri = r'\d{3}\s*[ - \.\s]\s*\d{3}\s*[ - \.\s]\s*\d{4}|\(\d{3}\)\s*\d{3}\s*[ - \.\s]\d{4}|\d{3}[ - \.\s]\d{4}'
-        result = re.findall(ri,text)
-        ph = re.compile('''((?:(?<![\d-])(?:\+?\d{1,3}[-.\s*]?)?(?:\(?\d{3}\)?[-.\s*]?)?\d{3}[-.\s*]?\d{4}(?![\d-]))|(?:(?<![\d-])(?:(?:\(\+?\d{2}\))|(?:\+?\d{2}))\s*\d{2}\s*\d{3}\s*\d{4}(?![\d-])))''')
-        tr = ph.findall(text)
+        ri=r'\d{3}\s*[ - \.\s]\s*\d{3}\s*[ - \.\s]\s*\d{4}|\(\d{3}\)\s*\d{3}\s*[ - \.\s]\d{4}|\d{3}[ - \.\s]\d{4}'
+        result=re.findall(ri,text)
+        ph=re.compile('''((?:(?<![\d-])(?:\+?\d{1,3}[-.\s*]?)?(?:\(?\d{3}\)?[-.\s*]?)?\d{3}[-.\s*]?\d{4}(?![\d-]))|(?:(?<![\d-])(?:(?:\(\+?\d{2}\))|(?:\+?\d{2}))\s*\d{2}\s*\d{3}\s*\d{4}(?![\d-])))''')
+        tr=ph.findall(text)
         #print(tr)
         ts = ""
         for g in tr:
             for k in g:
-                ts = ts + k
+                ts=ts + k
         #print(ts)
-        result = result + list(ts)
+        result=result + list(ts)
         #print(result)
         for k in result:
-            text = text.replace(k,'\u2586')
-        idata[i] = text
+            text=text.replace(k,'\u2586')
+        idata[i]=text
         tdata.append(text)
     #print(tdata)
-    pcount = tdata[0].count('\u2586')
+    pcount=tdata[0].count('\u2586')
     #print(tdata,pcount)
     return tdata,pcount
 
 def Replace(char):
-    if char =='\n':
-        char = ""
+    if char=='\n':
+        char=""
     return char
 
 # Function for Reading address
 
 def Redact_address(idata):
     #print(idata)
-    #idata = list(filter(Replace,idata))
     tdata=[]
-    #nlp1=spacy.load("C:/Users/Admin/Desktop/address-parser-main/address-parser-main/output/models/model-best")
     for i,text in enumerate(idata):
-        text = re.sub(r'\n', '', text)
-        addr = re.compile('\d{1,4} [\w\s]{1,20}(?:main|street|st|avenue|ave|road|rd|highway|hwy|square|sq|trail|trl|drive|dr|court|ct|park|parkway|parkwood|pkwy|circle|cir|boulevard|blvd|Blvd)\W?(?=\s|$) | \b\d{5}(?:[-\s]\d{4})?\b', re.IGNORECASE)
-        temp_addr = addr.findall(text)
+        text=re.sub(r'\n', '', text)
+        addr=re.compile('\d{1,4} [\w\s]{1,20}(?:main|street|st|avenue|ave|road|rd|highway|hwy|square|sq|trail|trl|drive|dr|court|ct|park|parkway|parkwood|pkwy|circle|cir|boulevard|blvd|Blvd)\W?(?=\s|$) | \b\d{5}(?:[-\s]\d{4})?\b', re.IGNORECASE)
+        temp_addr=addr.findall(text)
         print(temp_addr)
         for k in temp_addr:
-            text = text.replace(k,'\u2585')
-        idata[i] = text
+            text=text.replace(k,'\u2585')
+        idata[i]=text
         tdata.append(text)
     #print(tdata)
-    acount = tdata[0].count('\u2585')
+    acount=tdata[0].count('\u2585')
     #print(tdata,acount)
     return tdata,acount
 
 # Function for Reading concept
 
 def Redact_concept(idata, concept):
-    tdata = []
-    syn = wordnet.synsets(concept)
-    synonyms = []
-    #print(syn)
+    tdata=[]
+    syn=wordnet.synsets(concept)
+    synonyms=[]
     for i in syn:
         for l in i.lemmas():
             synonyms.append(l.name())
@@ -150,45 +147,46 @@ def Redact_concept(idata, concept):
             synonyms.append(hyper.name())
         for hypo in i.hyponyms():
             synonyms.append(hypo.name())
-    #print(synonyms)
     for i in idata:
-        doc = nlp(i)
-        temp = []
+        doc=nlp(i)
+        temp=[]
         for t in doc:
-            test = lambda z : '\u2584' if str(t) in synonyms else z
+            test=lambda z:'\u2584' if str(t) in synonyms else z
             temp.append(test(t))
         tdata.append(" ".join([str(x) for x in temp]))
-    ccount = tdata[0].count('\u2584')
+    ccount=tdata[0].count('\u2584')
     #print(tdata,ccount)
     return tdata,ccount
+
+# Function for Writing Output to the Files
 
 def write_output(idata):
     outputfiles=[]
     for i in input_files:
-        newname = os.path.splitext(i)[0]
-        newname = newname +'.redacted.txt'
+        newname=os.path.splitext(i)[0]
+        newname=newname +'.redacted.txt'
         outputfiles.append(newname)
     #print(outputfiles)
 
     # Parent Directory path
-    parent_dir = str(os.getcwd())
+    parent_dir=str(os.getcwd())
     # Path
-    path = os.path.join(parent_dir, "files")
+    path=os.path.join(parent_dir,"files")
     try:
         os.mkdir(path)
     except:
         print("Directory Exists")
 
     for i in range(len(outputfiles)):
-        #print(i)
-        despath = str(os.getcwd())+"/files/"+outputfiles[i]
+        despath=str(os.getcwd())+"/files/"+outputfiles[i]
         with open(despath, 'w+' , encoding='utf-8') as file:
             file.write(idata[i])
             file.close()
 
-def write_stats(idata):
-    
-    cnt_names, cnt_gender, cnt_dates, cnt_address, cnt_phones, cnt_concept = [], [], [], [], [], []
+# Function writing the stats into File
+
+def write_stats(idata):    
+    cnt_names, cnt_gender, cnt_dates, cnt_address, cnt_phones, cnt_concept=[], [], [], [], [], []
     
     for t in idata:
         cnt_names.append(t.count('\u2588'))
@@ -198,20 +196,21 @@ def write_stats(idata):
         cnt_phones.append(t.count('\u2586'))
         cnt_concept.append(t.count('\u2584'))
     #print(cnt_names,cnt_gender,cnt_dates,cnt_phones,cnt_address,cnt_concept)
-     # Parent Directory path
-    parent_dir = str(os.getcwd())
-     # Path
-    path = os.path.join(parent_dir, "stderr")
+
+    # Parent Directory path
+    parent_dir=str(os.getcwd())
+    # Path
+    path=os.path.join(parent_dir, "stderr")
     try:
         os.mkdir(path)
     except:
         print("Directory Exists")
     
-    despath = str(os.getcwd())+"/stderr/"+"stderr.txt"
+    despath=str(os.getcwd())+"/stderr/"+"stderr.txt"
     with open(despath, 'w+' , encoding='utf-8') as file:
         file.write("Names: "+str(sum(cnt_names))+"\n")
         file.write("Dates: "+str(sum(cnt_dates))+"\n")
-        file.write("Phones: "+str(sum(cnt_phones))+"\n")
+        file.write("Phones Numbers: "+str(sum(cnt_phones))+"\n")
         file.write("Genders: "+str(sum(cnt_gender))+"\n")
         file.write("Address: "+str(sum(cnt_address))+"\n")
         file.write("Concepts: "+str(sum(cnt_concept)))
